e45048fb250f99de0988994b15a632a22536bd72
Xavier Raffin <xavierraffin@users.noreply.github.com>
2022-11-22 19:02:28 +0000

    Now favor simple kill chance and avoid head to heath death chance

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"math/rand"
 )
 
+  var DEATH_PENALTY = -100
+  var KILL_BONUS = 1
+
 // info is called when you create your Battlesnake on play.battlesnake.com
 // and controls your Battlesnake's appearance
 // TIP: If you open your Battlesnake URL in a browser you should see this data
@@ -34,11 +37,77 @@ func end(state GameState) {
 	log.Printf("GAME OVER\n\n")
 }
 
-/*
-func killOrAvoid(myHead Coord, bodyPart Coord, isMoveSafe map[string]bool, enemyLength int, myLength int) {
-  if()
+
+func killOrAvoid(myHead Coord, otherHead Coord, isMoveBeneficial map[string]int, enemyLength int, myLength int, width int, height int) {
+  var colisionScore = KILL_BONUS
+  if(enemyLength >= myLength) {
+    colisionScore = DEATH_PENALTY
+  }
+  if GAME_MODE != Wrapped {
+    if(myHead.X == otherHead.X){
+      if(myHead.Y + 2 == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+      } else if(myHead.Y - 2 == otherHead.Y) {
+        isMoveBeneficial["down"] += colisionScore
+      }
+    } else if(myHead.Y == otherHead.Y){
+      if(myHead.X + 2 == otherHead.X) {
+        isMoveBeneficial["right"] += colisionScore
+      } else if(myHead.X - 2 == otherHead.X) {
+        isMoveBeneficial["left"] += colisionScore
+      }
+    }
+    if(myHead.X == otherHead.X + 1){
+      if(myHead.Y + 1 == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+        isMoveBeneficial["left"] += colisionScore
+      } else if(myHead.Y - 1 == otherHead.Y) {
+        isMoveBeneficial["down"] += colisionScore
+        isMoveBeneficial["left"] += colisionScore
+      }
+    } else if(myHead.X == otherHead.X - 1){
+      if(myHead.Y + 1 == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+        isMoveBeneficial["right"] += colisionScore
+      } else if(myHead.Y - 1 == otherHead.Y) {
+        isMoveBeneficial["down"] += colisionScore
+        isMoveBeneficial["right"] += colisionScore
+      }
+    }
+  } else {
+    if(myHead.X == otherHead.X){
+      if((myHead.Y + 2)%height == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+      } else if((myHead.Y - 2)%height == otherHead.Y) {
+        isMoveBeneficial["down"] += colisionScore
+      }
+    } else if(myHead.Y == otherHead.Y){
+      if((myHead.X + 2)%width == otherHead.X) {
+        isMoveBeneficial["right"] += colisionScore
+      } else if(myHead.X == (otherHead.X + 2)%width) {
+        isMoveBeneficial["left"] += colisionScore
+      }
+    }
+    if(myHead.X == (otherHead.X + 1)%width){
+      if((myHead.Y + 1)%height == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+        isMoveBeneficial["left"] += colisionScore
+      } else if(myHead.Y == (otherHead.Y + 1)%height) {
+        isMoveBeneficial["down"] += colisionScore
+        isMoveBeneficial["left"] += colisionScore
+      }
+    } else if((myHead.X +1)%width == otherHead.X){
+      if((myHead.Y + 1)%height  == otherHead.Y) {
+        isMoveBeneficial["up"] += colisionScore
+        isMoveBeneficial["right"] += colisionScore
+      } else if(myHead.Y == (otherHead.Y + 1)%height) {
+        isMoveBeneficial["down"] += colisionScore
+        isMoveBeneficial["right"] += colisionScore
+      }
+    }
+  }
 }
-*/
+
 
 func avoidCollision(myHead Coord, bodyPart Coord, isMoveSafe map[string]bool, width int, height int) {
   if GAME_MODE != Wrapped {
@@ -59,7 +128,7 @@ func avoidCollision(myHead Coord, bodyPart Coord, isMoveSafe map[string]bool, wi
     if(myHead.X == bodyPart.X){
       if((myHead.Y + 1)%height == bodyPart.Y) {
         isMoveSafe["up"] = false
-      } else if((myHead.Y - 1)%height == bodyPart.Y) {
+      } else if(myHead.Y == (bodyPart.Y +1)%height) {
         isMoveSafe["down"] = false
       }
     } else if(myHead.Y == bodyPart.Y){
@@ -85,6 +154,12 @@ func move(state GameState) BattlesnakeMoveResponse {
 		"down":  true,
 		"left":  true,
 		"right": true,
+	}
+  isMoveBeneficial := map[string]int{
+		"up":    0,
+		"down":  0,
+		"left":  0,
+		"right": 0,
 	}
 
 	// We've included code to prevent your Battlesnake from moving backwards
@@ -113,7 +188,7 @@ func move(state GameState) BattlesnakeMoveResponse {
     if(snake.ID != state.You.ID) {
       // Next turn the head will be replaced by neck
       avoidCollision(myHead, snake.Body[0], isMoveSafe, state.Board.Width, state.Board.Height)
-      // killOrAvoid(myHead, snake.Body[0], isMoveSafe, snake.Length, state.You.Length)
+      killOrAvoid(myHead, snake.Body[0], isMoveBeneficial, snake.Length, state.You.Length, state.Board.Width, state.Board.Height)
     }
     for j := 1; j < len(snake.Body) - 1; j++ { // We ignore the queue, it will move
       avoidCollision(myHead, snake.Body[j], isMoveSafe, state.Board.Width, state.Board.Height)
@@ -134,11 +209,32 @@ func move(state GameState) BattlesnakeMoveResponse {
 		return BattlesnakeMoveResponse{Move: "down"}
 	}
 
+  // Remove safeMoves with a lower benefit
+  var maxMoveBenefits int = isMoveBeneficial[safeMoves[0]];
+  var currentBenefits int
+  for j := 1; j < len(safeMoves); j++ {
+    currentBenefits = isMoveBeneficial[safeMoves[j]];
+    if(currentBenefits > maxMoveBenefits) {
+      maxMoveBenefits = currentBenefits;
+    }
+  }
+
+  bestMoves := []string{}
+  for j := 0; j < len(safeMoves); j++ {
+    currentBenefits = isMoveBeneficial[safeMoves[j]];
+    if(currentBenefits == maxMoveBenefits) {
+      bestMoves = append(bestMoves, safeMoves[j])
+    }
+  }
+  
 	// Choose a random move from the safe ones
-	nextMove := safeMoves[rand.Intn(len(safeMoves))]
+	nextMove := bestMoves[rand.Intn(len(bestMoves))]
 
 	// TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
 	// food := state.Board.Food
+  printObj(bestMoves)
+  printObj(isMoveBeneficial)
+  printObj(isMoveSafe)
 
 	log.Printf("MOVE %d: %s\n", state.Turn, nextMove)
 	return BattlesnakeMoveResponse{Move: nextMove}
